9bc6353925943895103d0208c32b16a4177cee0e
Darkczc <1017785726ccc0@sina.com>
2018-03-07 18:02:56 +0800

    2018-03-07

--- blog/views.py
+++ blog/views.py
@@ -4,3 +4,6 @@ import datetime
 def index(request):
     return render(request,template_name='index.html')
 # Create your views here.
+
+def blog(request,blog_id):
+    return render(request,template_name='blog.html')
